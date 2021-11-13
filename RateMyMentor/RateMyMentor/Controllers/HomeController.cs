dc5d7398b57e957fb85aa2b121f17c4c97e3c3f9
Lucie Yarish <lucieyarish@hotmail.com>
2021-11-13 18:47:54 +0100

    Add new mentor endpoint working

--- RateMyMentor/RateMyMentor/Controllers/HomeController.cs
+++ RateMyMentor/RateMyMentor/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
+using RateMyMentor.Models.Entities;
 using RateMyMentor.Services;
+using RateMyMentor.ViewModels;
 
 namespace RateMyMentor.Controllers
 {
+    [Route("")]
     public class HomeController : Controller
     {
         private MentorService MentorService { get; }
@@ -10,6 +13,35 @@ namespace RateMyMentor.Controllers
         {
             MentorService = service;
         }
+
+        [HttpGet("")]
+        public IActionResult ListAll()
+        {
+            var result = new MentorViewModel()
+            {
+                Mentors = MentorService.FindAll()
+            };
+            return View("Index", result);
+        }
+
+        [HttpGet("mentor/{id:long}")]
+        public IActionResult GetMentor([FromQuery] long id)
+        {
+            var foundMentor = MentorService.FindById(id);
+            var mentor = new MentorViewModel()
+            {
+                Mentor = foundMentor
+            };
+            return View("MentorDetail", mentor);
+        }
         
+        [HttpPost("mentor")]
+        public IActionResult SaveMentor(Mentor mentor)
+        {
+            MentorService.CreateMentor(mentor);
+            return RedirectToAction("ListAll");
+        }
+
+       
     }
 }
--- RateMyMentor/RateMyMentor/Services/MentorService.cs
+++ RateMyMentor/RateMyMentor/Services/MentorService.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+using RateMyMentor.Models.Entities;
 using RateMyMentor.Persistence;
 
 namespace RateMyMentor.Services
@@ -9,6 +12,28 @@ namespace RateMyMentor.Services
         public MentorService(ApplicationDbContext dbContext)
         {
             DbContext = dbContext;
+        }
+
+        public List<Mentor> FindAll()
+        {
+            return DbContext.Mentors.ToList();
+        }
+
+        public Mentor FindById(long id)
+        {
+            var foundMentor =
+                FindAll()
+                    .Where(m => m.Id == id)
+                    .ToList()
+                    .FirstOrDefault();
+            return foundMentor;
+        }
+
+        public Mentor CreateMentor(Mentor mentor)
+        {
+            var savedMentor = DbContext.Mentors.Add(mentor).Entity;
+            DbContext.SaveChanges();
+            return savedMentor;
         }
     }
 }
